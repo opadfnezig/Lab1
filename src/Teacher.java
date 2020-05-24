7618d8fbdf9a1e3c6db635d830bb011f5ef9d7a1
Opadfnezig <shlopka18@gmail.com>
2020-05-24 18:13:03 +0300

    added Base

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,21 +1,16 @@
 
 public class Teacher {
 	private String name;
-	
+	private String subject;
+	public Teacher() {}
 	public Teacher(String name)
 	{
 		setName(name);
 	}
 	
-	public void setName(String n)
-	{
-		name = n;
-	}
-	
-	public String getName()
-	{
-		return name;
-	}
-	
+	public void setName(String n) {	name = n;}	
+	public String getName() { return name; }
+	public void setSubject(String subject) { this.subject = subject; }
+	public String getSubject() { return subject; }
 	public String toString() { return name; }
 }
