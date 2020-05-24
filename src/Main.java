7f485e58c7039e784c49c3101db5eb926de79e51
ThorIgor <44617152+ThorIgor@users.noreply.github.com>
2020-05-24 12:33:03 +0300

    start of base methods

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ public class Main {
 		Faculty faculties[] = new Faculty[0];
 		while(!end)
 		{
-			help();
-			break;
+			
 		}
 	}
 	
@@ -30,11 +29,129 @@ public class Main {
 		System.out.println("Спосіб використання: sort [/O] [назва/ID] [/B]");
 		System.out.println("/O - обєкт(faculty, department)");
 		System.out.println("/B - сортувати за(name, course)");
+		System.out.println();
+		System.out.println("appoint - команда назначення студентів та викладачів до групи");
+		System.out.println("appoint [/O] [назва] [ID]");
+		System.out.println("/O - обєкт(student, teacher)");
 	}
 	
 	private static void add(String str)
 	{
-		
+		String com[] = dellEmpty(str.split(" "));
+		switch(com[0])
+		{
+		case "faculty":
+			break;
+		case "department":
+			break;
+		case "student":
+			break;
+		case "teacher":
+			break;
+		case "group":
+			break;
+		default:
+			System.out.println("Error");
+			return;
+		}
 	}
-
+	
+	private static void print(String str)
+	{
+		String com[] = dellEmpty(str.split(" "));
+		switch(com[0])
+		{
+		case "faculty":
+			break;
+		case "department":
+			break;
+		case "student":
+			break;
+		case "teacher":
+			break;
+		case "group":
+			break;
+		case "course":
+			break;
+		default:
+			System.out.println("Error");
+			return;
+		}
+	}
+	
+	private static void search(String str)
+	{
+		String com[] = dellEmpty(str.split(" "));
+		switch(com[0])
+		{
+		case "faculty":
+			break;
+		case "department":
+			break;
+		case "student":
+			break;
+		case "teacher":
+			break;
+		case "group":
+			break;
+		default:
+			System.out.println("Error");
+			return;
+		}
+	}
+	
+	private static void sort(String str)
+	{
+		String com[] = dellEmpty(str.split(" "));
+		switch(com[0])
+		{
+		case "faculty":
+			break;
+		case "department":
+			break;
+		default:
+			System.out.println("Error");
+			return;
+		}
+	}
+	
+	private static void appoint(String str)
+	{
+		String com[] = dellEmpty(str.split(" "));
+		switch(com[0])
+		{
+		case "student":
+			break;
+		case "teacher":
+			break;
+		default:
+			System.out.println("Error");
+			return;
+		}
+	}
+	
+	private static String[] dellEmpty(String[] str)
+	{
+		String newStr[] = new String[str.length];
+		int count = 0;
+		for(int i = 0; i < str.length; i++)
+		{
+			if(str[i] == "" || str[i] == null)
+			{
+				newStr = reduceArr(newStr);
+				count++;
+			}
+			else
+				newStr[i-count] = str[i];
+		}
+		return str;
+	}
+	
+	private static String[] reduceArr(String[] str)
+	{
+		String newStr[] = new String[str.length-1];
+		for(int i = 0; i < newStr.length; i++)
+			newStr[i] = str[i];
+		return newStr;
+	}
 }
